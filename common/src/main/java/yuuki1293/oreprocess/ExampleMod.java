92e6382f1a4bd7b7d8121e3ec5ff5f83fe533a37
yuuki1293 <yuuki1293@outlook.jp>
2024-12-09 08:25:49 +0900

    first commit

---
+++ common/src/main/java/yuuki1293/oreprocess/ExampleMod.java
@@ -0,0 +1,9 @@
+package yuuki1293.oreprocess;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "oreprocess";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/yuuki1293/oreprocess/neoforge/ExampleModNeoForge.java
@@ -0,0 +1,13 @@
+package yuuki1293.oreprocess.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import yuuki1293.oreprocess.ExampleMod;
+
+@Mod(ExampleMod.MOD_ID)
+public final class ExampleModNeoForge {
+    public ExampleModNeoForge() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
